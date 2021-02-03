df2e2fa06c76e78fe824753c211e30ac0941bf6a
Steve99-coder <stevenderitu99@gmail.com>
2021-02-03 22:07:14 +0300

    Add models for Event

--- eventapp/models.py
+++ eventapp/models.py
@@ -25,3 +25,39 @@ class Profile(models.Model):
     def __str__(self):
         return self.user
 
+class Event(models.Model):
+
+    title = models.CharField(max_length =60)
+    image=models.ImageField(upload_to = 'pic/', blank=False, null=False)
+    description=HTMLField()
+    link=models.URLField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE,)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True, null=True)
+    comments=models.CharField(max_length=100, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def update_description(cls,id,description):
+        description=cls.objects.filter(description_id=id).update(description=description)
+        
+        return description
+
+    @classmethod
+    def get_projects(cls):
+        projects=cls.objects.all().prefetch_related('comment_set')
+        return projects
+
+    @classmethod
+    def search_project(cls,title):
+        title = cls.objects.filter(title__icontains=title)
+        return title
