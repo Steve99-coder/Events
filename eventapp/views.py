5c1b3e2dbc30e982d7aa22616df33dc83742aab2
Steve99-coder <stevenderitu99@gmail.com>
2021-02-03 22:28:31 +0300

    Add the welcome view function

--- eventapp/views.py
+++ eventapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def welcome(request):
+    current_user = request.user
+    user_profile= Profile.objects.filter(user=current_user.id).first()
+    comment= Comment.objects.filter(user=current_user.id).first()
+    event = Event.objects.all()
+
+   
+    return render(request, 'users/index.html', {'user_profile':user_profile, 'events':events,'comment':comment})
