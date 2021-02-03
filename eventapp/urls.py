5129bf4679ac61b8a568413b576a255a7043756b
Steve99-coder <stevenderitu99@gmail.com>
2021-02-03 23:56:20 +0300

    Add the url for search results

--- eventapp/urls.py
+++ eventapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns=[
     url(r'^profile$', views.user_profile, name='user-profile'),
     url(r'^$', views.welcome, name="welcome"),
     url(r'^edit/profile$', views.edit_profile, name="edit-profile"),
-   
+    url(r'^search/', views.search_results, name='search_results'),
+    
 ]
