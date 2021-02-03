dce9f40dcda057b1be4682b3b35ab336e86bfbcb
Steve99-coder <stevenderitu99@gmail.com>
2021-02-03 23:57:10 +0300

    Add the url for new event

--- eventapp/urls.py
+++ eventapp/urls.py
@@ -6,5 +6,7 @@ urlpatterns=[
     url(r'^$', views.welcome, name="welcome"),
     url(r'^edit/profile$', views.edit_profile, name="edit-profile"),
     url(r'^search/', views.search_results, name='search_results'),
+    url(r'^newcomment/(\d+)/$', views.new_comment, name='new-comment'),
+    url(r'^new/event$',views.new_event, name ='new-project'),
     
 ]
